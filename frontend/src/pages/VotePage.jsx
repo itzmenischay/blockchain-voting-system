9c70b8d0b431ff1c3b8ff0d5083d7ce09e4a593c
Nischay M <gladiatoruk1234@gmail.com>
2026-05-03 02:20:38 +0530

    Metamask wallet connection implemented

--- frontend/src/pages/VotePage.jsx
+++ frontend/src/pages/VotePage.jsx
@@ -1,33 +1,73 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import { generateVoteHash } from "../utils/hash.js";
 import { submitVote } from "../services/voteService.js";
+import { connectWallet } from "../utils/wallet.js";
 
 const VotePage = () => {
-  const handleVote = async (candidate) => {
-    try {
-      const walletAddress = "demo-wallet"; // temporary
+  const [wallet, setWallet] = useState(null);
+  const [status, setStatus] = useState("");
 
-      const { hash } = generateVoteHash(candidate, walletAddress);
+  const candidates = ["Candidate A", "Candidate B"];
 
-      console.log("Generated Hash:", hash);
-
-      const response = await submitVote(hash);
-
-      console.log("Backend Response:", response);
-
-      alert("Vote submitted!"); // to be replaced with react-toast
-    } catch (error) {
-      console.error(error);
-      alert("Error submitting vote"); // to be replaced with react-toast
+  const handleConnectWallet = async () => {
+    const address = await connectWallet();
+    if (address) {
+      setWallet(address);
+      setStatus("Wallet Connected!");
     }
   };
-  
+
+  const handleVote = async (candidate) => {
+    try {
+      if (!wallet) {
+        setStatus("Please connect wallet first");
+        return;
+      }
+
+      setStatus("Submitting vote...");
+
+      const { hash } = generateVoteHash(candidate, wallet);
+
+      const res = await submitVote(hash);
+      console.log(res);
+      setStatus("Vote submitted successfully!");
+    } catch (error) {
+      setStatus("Error submitting vote!");
+    }
+  };
+
+  useEffect(() => {
+    console.log("Ethereum object: ", window.ethereum);
+  }, []);
+
+  const testWallet = async () => {
+    const address = await connectWallet();
+    console.log("Connected wallet: ", address);
+  };
+
   return (
-    <div>
-      Vote Page
-      <button onClick={handleVote}>Click Me!</button>
-    </div>
+    <>
+      <div>
+        <h2>Vote Page</h2>
+        {candidates.map((candidte) => {
+          <div key={candidte} style={{ marginBottom: "10px" }}>
+            <span>{candidte}</span>
+            <button
+              onClick={() => handleVote(candidte)}
+              style={{ marginLeft: "10px", font: "black"}}
+            >
+              Vote
+            </button>
+          </div>;
+        })}
+
+        {/* status */}
+        <p style={{ marginTop: "20px" }}>{status}</p>
+        <button onClick={testWallet}>Connect Wallet</button>
+      </div>
+    </>
   );
 };
 
